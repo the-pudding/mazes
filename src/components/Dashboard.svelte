cc4d4a5ea90e595b9e71d35d3771c71ed3aeeecf
Michelle McGhee <michmac202@gmail.com>
2023-06-02 14:16:28 -0700

    map with css grid

---
+++ src/components/Dashboard.svelte
@@ -0,0 +1,38 @@
+<script>
+	import states from "$data/states.json";
+</script>
+
+<figure>
+	{#each states as { id, state, row, col }}
+		<div
+			class="state"
+			id={id.toLowerCase()}
+			style={`--row: ${row}; --col: ${col}`}
+		>
+			<div class="abbrev">{id}</div>
+			<img src={"assets/img/iowa-100.png"} />
+		</div>
+	{/each}
+</figure>
+
+<style>
+	figure {
+		display: grid;
+		grid-template-columns: repeat(12, 1fr);
+		grid-template-rows: repeat(8, 1fr);
+		gap: 1%;
+		max-width: 1000px;
+		margin: auto;
+	}
+	.state {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		grid-row: var(--row);
+		grid-column: var(--col);
+	}
+	.abbrev {
+		font-family: var(--sans);
+		color: var(--color-gray-400);
+	}
+</style>
--- src/components/Index.svelte
+++ src/components/Index.svelte
@@ -1,13 +1,5 @@
 <script>
-	import { getContext } from "svelte";
-	import Demo from "$components/demo/Demo.svelte";
-	import WIP from "$components/helpers/WIP.svelte";
-	// import Footer from "$components/Footer.svelte";
-
-	// const copy = getContext("copy");
-	// const data = getContext("data");
+	import Dashboard from "$components/Dashboard.svelte";
 </script>
 
-<WIP />
-<Demo />
-<!-- <Footer /> -->
+<Dashboard />
