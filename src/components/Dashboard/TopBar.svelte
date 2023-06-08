25365a10069268b25a1106177c6723aac41454b2
Michelle McGhee <michmac202@gmail.com>
2023-06-08 14:53:46 -0700

    build

--- src/components/Dashboard/TopBar.svelte
+++ src/components/Dashboard/TopBar.svelte
@@ -2,6 +2,7 @@
 	import Select from "$components/helpers/Select.svelte";
 	import states from "$data/states.csv";
 	import { getContext } from "svelte";
+	import { selectedState } from "$stores/misc.js";
 
 	const { getOrder } = getContext("dashboard");
 	const order = getOrder();
@@ -14,11 +15,22 @@
 		{ label: "by fewer barriers", value: "barriers-asc" }
 	];
 	const highlightOptions = states.map((d) => ({ label: d.name, value: d.id }));
+
+	// TODO: make a seperate variable here, goToState
+	// when i select something, it updates $selectedState
+
+	// when i open modal, focus should go in modal
+	// when i close modal, focus should go to the state el that was just selected
+	// focus looks like a pink outline
 </script>
 
 <div class="top-bar">
 	<Select label="Order" options={orderOptions} bind:value={$order} />
-	<!-- <Select label="Highlight" options={highlightOptions} bind:value={highlight} /> -->
+	<Select
+		label="See state maze for"
+		options={highlightOptions}
+		bind:value={$selectedState}
+	/>
 </div>
 
 <style>
