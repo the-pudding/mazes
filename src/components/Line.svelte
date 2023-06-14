c044adf32c547ad041a8ee6bf9877eea07b2e809
Michelle McGhee <michmac202@gmail.com>
2023-06-14 16:55:07 -0700

    basic line following scroll

--- src/components/Line.svelte
+++ src/components/Line.svelte
@@ -1,20 +1,26 @@
 <script>
 	import { onMount } from "svelte";
+	import scrollY from "$stores/scrollY.js";
+	import _ from "lodash";
+	import { scaleLinear } from "d3";
 
 	const padding = 15;
 	let start;
 	let height;
 	let width;
 	let heights = [];
+	let pathLength;
+	$: dashScale = scaleLinear().domain([0, 4500]).range([pathLength, 0]); // TODO should be max scrollY
+	$: currentDashOffset = dashScale($scrollY);
 
 	onMount(() => {
 		const div = document.querySelector("div.sections");
 		height = div.clientHeight;
 		width = div.clientWidth;
 		const chunks = document.querySelectorAll("section > .chunk, section > h2");
-		console.log({ chunks });
 		start = chunks[0].offsetTop;
 		heights = Array.from(chunks).map((chunk) => chunk.clientHeight);
+		pathLength = _.sum(heights) + (width - padding * 2) * heights.length;
 	});
 
 	$: pathStr = `M ${padding} ${start} ${heights.map(
@@ -25,7 +31,10 @@
 
 {#if height}
 	<svg width="100%" {height}>
-		<path d={pathStr} />
+		<path
+			d={pathStr}
+			style={`--pathLength: ${pathLength}px; --offset: ${currentDashOffset}`}
+		/>
 	</svg>
 {/if}
 
@@ -40,5 +49,7 @@
 		stroke-width: 3px;
 		opacity: 0.5;
 		fill: none;
+		stroke-dasharray: var(--pathLength);
+		stroke-dashoffset: var(--offset);
 	}
 </style>
