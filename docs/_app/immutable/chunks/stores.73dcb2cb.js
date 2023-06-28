a825b7ea793216c8520d1e41093e0585efeb6eb8
Michelle McGhee <michmac202@gmail.com>
2023-06-27 19:01:30 -0700

    update github pages

--- docs/_app/immutable/chunks/stores.73dcb2cb.js
+++
@@ -1,1 +0,0 @@
-import"./index.efe9b594.js";import{s as e}from"./singletons.5c72bfe7.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
